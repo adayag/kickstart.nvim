bafdc596a4bb842a2df3f54a09b6fbc0b9b9ba40
Allen Dayag <allen.dayag@madeintandem.com>
2026-02-10 19:36:48 -0600

    add Ruby and TypeScript LSPs, buffer tab cycling
    - Configure ruby_lsp and ts_ls language servers
    - Add ruby-lsp and typescript-language-server to Mason
    - Fix Mason ensure_installed to use correct package names
    - Add Tab/Shift+Tab keymaps to cycle buffers

--- init.lua
+++ init.lua
@@ -193,6 +193,10 @@ vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('v', '<', '<gv')
 vim.keymap.set('v', '>', '>gv')
 
+-- Cycle buffers with Tab / Shift+Tab
+vim.keymap.set('n', '<Tab>', ':bnext<CR>', { silent = true, desc = 'Next buffer' })
+vim.keymap.set('n', '<S-Tab>', ':bprev<CR>', { silent = true, desc = 'Previous buffer' })
+
 -- Strip trailing whitespace on save
 vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = '*',
@@ -626,16 +630,8 @@ require('lazy').setup({
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --  See `:help lsp-config` for information about keys and how to configure
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ruby_lsp = {},
+        ts_ls = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -645,12 +641,12 @@ require('lazy').setup({
       --    :Mason
       --
       -- You can press `g?` for help in this menu.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
+      local ensure_installed = {
         'lua-language-server', -- Lua Language server
         'stylua', -- Used to format Lua code
-        -- You can add other tools here that you want Mason to install
-      })
+        'ruby-lsp', -- Ruby language server
+        'typescript-language-server', -- TypeScript/JavaScript language server
+      }
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
