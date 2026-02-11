5d529fc097bd966fc8237050c1262fcf7fe03035
Allen Dayag <allen.dayag@madeintandem.com>
2026-02-10 23:15:13 -0600

    add which-key delay and telescope live grep args

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -40,6 +40,33 @@ return {
     },
   },
 
+  -- Telescope live grep with args (filter by file type: "foo -t ruby")
+  {
+    'nvim-telescope/telescope-live-grep-args.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim' },
+    config = function()
+      local telescope = require 'telescope'
+      local lga_actions = require 'telescope-live-grep-args.actions'
+      telescope.setup {
+        extensions = {
+          live_grep_args = {
+            auto_quoting = true,
+            mappings = {
+              i = {
+                ['<C-k>'] = lga_actions.quote_prompt(),
+                ['<C-i>'] = lga_actions.quote_prompt { postfix = ' --iglob ' },
+              },
+            },
+          },
+        },
+      }
+      telescope.load_extension 'live_grep_args'
+      vim.keymap.set('n', '<leader>sG', function()
+        require('telescope').extensions.live_grep_args.live_grep_args()
+      end, { desc = '[S]earch by [G]rep (with args)' })
+    end,
+  },
+
   -- Statusline
   {
     'nvim-lualine/lualine.nvim',
